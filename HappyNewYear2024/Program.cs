6599dd2dab27ccd3e60c05de5c4ba0567c714715
TANHUNG <TANHUNG@DESKTOP-QV5B92B>
2024-02-12 21:25:47 +0700

    Add project files.

---
+++ HappyNewYear2024/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace HappyNewYear2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Happy New Year 2024!");
+            Console.WriteLine("Chúc mừng năm mới");
+        }
+    }
+}
